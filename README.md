9b343b3491ef1c3df91edb7c4e81803a14608465
AliOlkac <aliolkac43@gmail.com>
2023-05-16 20:17:15 +0300

    Herhangi bir şey

---
+++ README.md
@@ -0,0 +1,31 @@
+# Wedding Landing Page - Shahirrah & Zamier (Work in Progress)
+
+![Thumbnail](image/initials.png)
+
+
+# Section/Feature
+- Main Info
+- Bottom Nav
+- Countdown to D-Day
+- Time and Place Info
+- Add to Calendar Button (Google Calendar)
+- Map Direction Button (Google Map)
+- Send Message Button (Whatssapp API)
+
+# Stack
+- Bulma CSS (https://bulma.io)
+
+# Info
+- Live version check at: https://weddinginvitationshazamier.netlify.app/
+
+# Fonts
+- Rouge Script (Google Font)
+- Raleway (Google Font)
+- Cormorant Garamond (Google Font)
+
+
+# Credits
+
+- Floral vector created by BiZkettE1 - www.freepik.com (https://www.freepik.com/free-photos-vectors/background)
+- plant-background captured by Milad Fakurian - unsplash.com (https://unsplash.com/photos/dPTvB0GePMI)
+- Code adapted from daengdoang - github.com (https://github.com/daengdoang/daeng-sherly-menikah), Personal Site (https://dae.ng/)
---
+++ js/menikah.js
@@ -0,0 +1,84 @@
+// Get that hamburger menu cookin' //
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+// Hide/Show Button
+function showQR() {
+  var x = document.getElementById("duitNow");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
+}
+
+// Hide/Show Dropdown
+// var dropdown = document.querySelector('.dropdown');
+// dropdown.addEventListener('click', function(event) {
+//   event.stopPropagation();
+//   dropdown.classList.toggle('is-active');
+// });
+function showDropdown() {
+  var Dropdown = document.getElementById("dropdown-menu2");
+  if (Dropdown.style.display === "none") {
+    Dropdown.style.display = "block";
+  }
+  else {
+    Dropdown.style.display = "none";
+  }
+}
+
+// Smooth Anchor Scrolling
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
+// When the user scrolls down 20px from the top of the document, show the scroll up button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
+}
+
+// Preloader
+$(document).ready(function($) {
+  $(".preloader-wrapper").fadeOut();
+  $("body").removeClass("preloader-site");
+});
+$(window).load(function() {
+  var Body = $("body");
+  Body.addClass("preloader-site");
+});
